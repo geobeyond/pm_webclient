2a13c99420309812c88cbc69ae9c41937e5abf01
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2014-06-11 19:57:48 +0200

    ushahidi based auth from external performed by pcasciano

--- index.php
+++ index.php
@@ -1,16 +1,73 @@
 <?php
+
+define('SYSPATH', '/var/www/ushahidi-v2/system/'); //cambia il path..
+
+require('../ushahidi-v2/application/config/auth.php');
+
+
 $sid=session_id();
 if ($sid=="") {
 	session_start();
 	$sid=session_id();
 }
 
+//funzione da usare per trovare il salt  di una pwd
+function find_salt($password, $salt_pattern)
+        {
+                $salt = '';
+
+                foreach ($salt_pattern as $i => $offset)
+                {
+                        // Find salt characters, take a good long look...
+                        $salt .= substr($password, $offset + $i, 1);
+                }
+                return $salt;
+        }
+
+
+
+//crea hash password
+function hash_password($password, $salt, $salt_pattern)
+{
+        $hash = hash('sha1', $salt.$password);
+
+                // Change salt to an array
+                $salt = str_split($salt, 1);
+
+                // Returned password
+                $password = '';
+
+                // Used to calculate the length of splits
+                $last_offset = 0;
+
+                foreach ($salt_pattern as $offset)
+                {
+                        // Split a new part of the hash off
+                        $part = substr($hash, 0, $offset - $last_offset);
+
+                        // Cut the current part out of the hash
+                        $hash = substr($hash, $offset - $last_offset);
+
+                        // Add the part to the password, appending the salt character
+                        $password .= $part.array_shift($salt);
+
+                        // Set the last offset to the current offset
+                        $last_offset = $offset;
+                }
+
+                // Return the password, with the remaining hash appended
+                return $password.$hash;
+}
+
+
+
 //$file_db = new PDO('sqlite:users.db');
-$file_db=new PDO('mysql:host=sql.geovolution.it;dbname=geovolut37572','geovolut37572','jack76');
+$file_db=new PDO('mysql:host=localhost;dbname=ushahidi_v2','root','lynxbinario');
 $loginPage=file_get_contents("loginPage.html");
 if (isset($_POST['username']) && isset($_POST['password'])){
-	$username=strtoupper($_POST['username']);
-	$password=strtoupper($_POST['password']);
+	$username=$_POST['username'];
+	//$password=md5($_POST['password']);
+	$password = $_POST['password'];
 	$hashKey=uniqid("placemaker-".$sid);
 	$result = $file_db->query("SELECT * FROM users where username='{$username}'");
 	$noResult=true;
@@ -18,18 +75,29 @@ if (isset($_POST['username']) && isset($_POST['password'])){
 		$noResult=false;
 	}
 	if (!$noResult){
-		if ($row['password']==$password){
+		
+		//trova il salt
+		$savedPassword=$row['password'];
+		$salt_pattern=explode(',', $config['salt_pattern']);
+		$salt = find_salt($savedPassword, $salt_pattern);
+                
+		//crea password con il salt trovato
+		$hash_pwd = hash_password($password, $salt, $salt_pattern);
+
+//		if ($row['password']==$password){
+		if ($savedPassword === $hash_pwd)
+		{
 			$update = "UPDATE users SET hashkey = '{$hashKey}'
 			WHERE username='{$username}'";
 			$file_db->exec($update);
 			setcookie("hashKey",$hashKey);
 			setcookie("username",$username);
 			header("location: index.php");
-		} else {
+		}else {
 			$loginPage=str_replace("<!--message-->", "<span class=\"warning\" style=\"color:red\">Username/password error</span>", $loginPage);
 			die($loginPage);
 		}
-	} else {
+	}else {
 		$loginPage=str_replace("<!--message-->", "<span class=\"warning\" style=\"color:red\">Username not found</span>", $loginPage);
 		die($loginPage);
 	}
